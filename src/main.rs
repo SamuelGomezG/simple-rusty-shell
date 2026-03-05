0f4ee3c4e18dc6217cc7d4549d83233bc5783023
Samuel Gómez Guio <samgomezgu@unal.edu.co>
2026-03-05 17:03:03 -0500

    Add loop for prompting the user and receiving a command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead, Write};
+
+fn main() -> io::Result<()> {
+    loop {
+        let mut stdout = io::stdout().lock();
+        stdout.write_all(b">> ")?;
+        stdout.flush()?;
+
+        let mut input_buffer = String::new();
+        let mut stdin = io::stdin().lock();
+        stdin.read_line(&mut input_buffer)?;
+
+        let command_string = input_buffer.trim();
+
+        if command_string == ":q" || command_string == "exit" {
+            break;
+        }
+    }
+
+    Ok(())
 }
